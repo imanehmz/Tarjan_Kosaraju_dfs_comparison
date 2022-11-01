1f39cfef5989994082ce525b1ab0dc5d87171658
imanehmz <ji_hamzaoui@esi.dz>
2022-11-01 21:53:26 +0100

    C version

--- new_tarjan.cpp
+++ new_tarjan.cpp
@@ -14,66 +14,171 @@ public:
 	//Meme en utilisant l'acces par index
 	// On déclare une liste des noeuds visités
 	// Elle est de type booléen car soit visité soit non visité
-	map<int, bool> visited;
+	map<int, int> V;
     // On déclare la liste des arcs, une liste d'adjacence de v accessible par la clé primaire v
 	map<int, list<int> > adj;
 
-	// Une fonction pour ajouter un arc au graphe
-	void addEdge(int v, int w);
-
-	//Parcours en profondeur à partir de v 
-	void DFS(int v, stack<int> *st);
-	//Une fonction qui retourne tout les composants fortement connectés
-	//n est le nombre de noeuds du graphe
-	void FindSCC(int n);
+	void SCCUtil(int u, int disc[], int low[],
+                stack<int> *st, bool stackMember[]);
+public:
+    Graph(map<int,int> V,map<int, list<int> > adj ) // Constructor
+    void addEdge(int v, int w); // function to add an edge to graph
+    void SCC(); // prints strongly connected components
 };
-
+ 
+Graph::Graph(map<int,int> V,map<int, list<int> > adj )
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+ 
 void Graph::addEdge(int v, int w)
 {
-	adj[v].push_back(w); // Add w to v’s list.
+    adj.find(v).push_back(w);
 }
-
-void Graph::DFS(int v,stack<int> *st)
+ 
+// A recursive function that finds and prints strongly connected
+// components using DFS traversal
+// u --> The vertex to be visited next
+// disc[] --> Stores discovery times of visited vertices
+// low[] -- >> earliest visited vertex (the vertex with minimum
+//             discovery time) that can be reached from subtree
+//             rooted with current vertex
+// *st -- >> To store all the connected ancestors (could be part
+//         of SCC)
+// stackMember[] --> bit/index array for faster check whether
+//                 a node is in stack
+void Graph::SCCUtil(int u, int disc[], int low[], stack<int> *st,
+                    bool stackMember[])
 {
-	//Visiter un noeud, en rendant le map de sa valeur a vrai
-	visited[v] = true;
-	cout << v << " ";
-	stack<int> stack; //Une pile qui va contenir 
-	//Iterer sur tout les neods adjacents du noeud visité
-	list<int>::iterator i;
-	//on parcours la map, la matrice d'adjacence de v du début à la fin avec list.begin() et list.end()
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-		//Si le noeud n'est pas encore visité on le visite récursivement
-		if (!visited[*i])
-			DFS(*i,st);
+    // A static variable is used for simplicity, we can avoid use
+    // of static variable by passing a pointer.
+    static int time = 0;
+ 
+    // Initialize discovery time and low value
+    disc[u] = low[u] = ++time;
+    st->push(u);
+    stackMember[u] = true;
+ 
+    // Go through all vertices adjacent to this
+    list<int>::iterator i;
+    for (i = adj[u].begin(); i != adj[u].end(); ++i)
+    {
+        int v = *i; // v is current adjacent of 'u'
+ 
+        // If v is not visited yet, then recur for it
+        if (disc[v] == -1)
+        {
+            SCCUtil(v, disc, low, st, stackMember);
+ 
+            // Check if the subtree rooted with 'v' has a
+            // connection to one of the ancestors of 'u'
+            // Case 1 (per above discussion on Disc and Low value)
+            low[u] = min(low[u], low[v]);
+        }
+ 
+        // Update low value of 'u' only of 'v' is still in stack
+        // (i.e. it's a back edge, not cross edge).
+        // Case 2 (per above discussion on Disc and Low value)
+        else if (stackMember[v] == true)
+            low[u] = min(low[u], disc[v]);
+    }
+ 
+    // head node found, pop the stack and print an SCC
+    int w = 0; // To store stack extracted vertices
+    if (low[u] == disc[u])
+    {
+        while (st->top() != u)
+        {
+            w = (int) st->top();
+            cout << w << " ";
+            stackMember[w] = false;
+            st->pop();
+        }
+        w = (int) st->top();
+        cout << w << "\n";
+        stackMember[w] = false;
+        st->pop();
+    }
 }
-void Graph::FindSCC(int n){
-	int i;
-	for(i = 0; i < n; ++i){
-		visited[i] = false	;
-	}
+ 
+// The function to do DFS traversal. It uses SCCUtil()
+void Graph::SCC()
+{
+    int *disc = new int[V];
+    int *low = new int[V];
+    bool *stackMember = new bool[V];
+    stack<int> *st = new stack<int>();
+ 
+    // Initialize disc and low, and stackMember arrays
+    for (int i = 0; i < V; i++)
+    {
+        disc[i] = NIL;
+        low[i] = NIL;
+        stackMember[i] = false;
+    }
+ 
+    // Call the recursive helper function to find strongly
+    // connected components in DFS tree with vertex 'i'
+    for (int i = 0; i < V; i++)
+        if (disc[i] == NIL)
+            SCCUtil(i, disc, low, st, stackMember);
 }
-
-// Driver's code
+ 
+// Driver program to test above function
 int main()
 {
-	// Create a graph given in the above diagram
-	Graph g;
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
-
-	cout << "Following is Depth First Traversal"
-			" (starting from vertex 2) \n";
-
-	// Appel de la fonction DFS en commençant pas le noeud portant la valeur 2
-	stack<int> *st = new stack<int>();
-	g.DFS(2, st);
-	int n = 10; // le nombre des neods du graphe
-	return 0;
+    cout << "\nSCCs in first graph \n";
+    Graph g1(5);
+    g1.addEdge(1, 0);
+    g1.addEdge(0, 2);
+    g1.addEdge(2, 1);
+    g1.addEdge(0, 3);
+    g1.addEdge(3, 4);
+    g1.SCC();
+ 
+    cout << "\nSCCs in second graph \n";
+    Graph g2(4);
+    g2.addEdge(0, 1);
+    g2.addEdge(1, 2);
+    g2.addEdge(2, 3);
+    g2.SCC();
+ 
+    cout << "\nSCCs in third graph \n";
+    Graph g3(7);
+    g3.addEdge(0, 1);
+    g3.addEdge(1, 2);
+    g3.addEdge(2, 0);
+    g3.addEdge(1, 3);
+    g3.addEdge(1, 4);
+    g3.addEdge(1, 6);
+    g3.addEdge(3, 5);
+    g3.addEdge(4, 5);
+    g3.SCC();
+ 
+    cout << "\nSCCs in fourth graph \n";
+    Graph g4(11);
+    g4.addEdge(0,1);g4.addEdge(0,3);
+    g4.addEdge(1,2);g4.addEdge(1,4);
+    g4.addEdge(2,0);g4.addEdge(2,6);
+    g4.addEdge(3,2);
+    g4.addEdge(4,5);g4.addEdge(4,6);
+    g4.addEdge(5,6);g4.addEdge(5,7);g4.addEdge(5,8);g4.addEdge(5,9);
+    g4.addEdge(6,4);
+    g4.addEdge(7,9);
+    g4.addEdge(8,9);
+    g4.addEdge(9,8);
+    g4.SCC();
+ 
+    cout << "\nSCCs in fifth graph \n";
+    Graph g5(5);
+    g5.addEdge(0,1);
+    g5.addEdge(1,2);
+    g5.addEdge(2,3);
+    g5.addEdge(2,4);
+    g5.addEdge(3,0);
+    g5.addEdge(4,2);
+    g5.SCC();
+ 
+    return 0;
 }
-
-// improved by Vishnudev C
